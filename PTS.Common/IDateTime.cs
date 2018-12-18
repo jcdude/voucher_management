2091f4eae9f4a09bb90580e57a399f75bc441d63
jed.fletcher <jed@gratistech.com>
2018-12-18 14:49:26 -0500

    Init commit

---
+++ PTS.Common/IDateTime.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PTS.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ PTS.Infrastructure/MachineDateTime.cs
@@ -0,0 +1,12 @@
+﻿using PTS.Common;
+using System;
+
+namespace PTS.Infrastructure
+{
+    public class MachineDateTime : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+
+        public int CurrentYear => DateTime.Now.Year;
+    }
+}
