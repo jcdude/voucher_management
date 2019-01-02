417544f0b79f5a9fc4b9121eddcbab122a285bea
jed.fletcher <jed@gratistech.com>
2019-01-02 17:04:22 -0500

    Added purchase method

---
+++ PTS.WebApi/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PTS.Application.Service.Commands;
+using PTS.Application.Service.Models;
+
+namespace PTS.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : BaseController
+    {
+        // POST api/customers
+        [HttpPost("/purchase")]
+        public async Task<ActionResult<PurchaseViewModel>> Purchase([FromBody]PurchaseCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
