07774f401bce4a2bbd9ad3b55621a8cc1df61fb4
jed.fletcher <jed@gratistech.com>
2019-01-03 21:25:41 -0500

    Added new migration to update the database

--- PTS.Persistence/Migrations/20190104022337_PTS_12312018_4.Designer.cs
+++ PTS.Persistence/Migrations/20190104022337_PTS_12312018_4.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PTS.Persistence;
@@ -9,9 +10,10 @@ using PTS.Persistence;
 namespace PTS.Persistence.Migrations
 {
     [DbContext(typeof(PTSDbContext))]
-    partial class PTSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190104022337_PTS_12312018_4")]
+    partial class PTS_12312018_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
